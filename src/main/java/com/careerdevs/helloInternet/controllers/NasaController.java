2ec9765d59696a260862169703ba485f29388c24
Derogene <96089543+Derogene@users.noreply.github.com>
2022-03-11 22:02:08 -0500

    NASA Controller work pre-bonus

--- src/main/java/com/careerdevs/helloInternet/controllers/NasaController.java
+++ src/main/java/com/careerdevs/helloInternet/controllers/NasaController.java
@@ -1,9 +1,6 @@
 package com.careerdevs.helloInternet.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -12,7 +9,7 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/nasa")
 public class NasaController {
 
-    private String myNasaKey = "DEMO_KEY";
+    private String myNasaKey = "4XmxGy6Fq2zeVNlU8Aj8OBIBl4sKrEcwvdpF9RSk";
     private String nasaApodEndpoint = "https://api.nasa.gov/planetary/apod?api_key=" + myNasaKey;
 
     @GetMapping("/apod")
